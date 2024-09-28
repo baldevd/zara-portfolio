832bd25f290baf3c656a442cb3ef11a84c855ccf
Baldev Desai <baldevd@live.unc.edu>
2024-09-28 16:55:22 -0400

    Styled components

--- src/containers/education/Educations.js
+++ src/containers/education/Educations.js
@@ -3,27 +3,35 @@ import "./Educations.css";
 import DegreeCard from "../../components/degreeCard/DegreeCard.js";
 import { degrees } from "../../portfolio";
 import { Fade } from "react-reveal";
+import styled from "styled-components";
 
-class Educations extends Component {
-  render() {
-    const theme = this.props.theme;
-    return (
-      <div className="main" id="educations">
-        <div className="educations-header-div">
-          <Fade bottom duration={2000} distance="20px">
-            <h1 className="educations-header" style={{ color: theme.text }}>
-              Degrees Received
-            </h1>
-          </Fade>
-        </div>
-        <div className="educations-body-div">
-          {degrees.degrees.map((degree) => {
-            return <DegreeCard degree={degree} theme={theme} />;
-          })}
-        </div>
+const EducationsContainer = styled.div``;
+
+const EducationsHeader = styled.div`
+  font-size: 40px;
+  line-height: 1.1;
+  font-family: "Google Sans Regular";
+  text-align: center;
+`;
+
+const Educations = (props) => {
+  const theme = props.theme;
+  return (
+    <div className="main" id="educations">
+      <div className="educations-header-div">
+        <Fade bottom duration={2000} distance="20px">
+          <h1 className="educations-header" style={{ color: theme.text }}>
+            Degrees Received
+          </h1>
+        </Fade>
       </div>
-    );
-  }
-}
+      <div className="educations-body-div">
+        {degrees.degrees.map((degree) => {
+          return <DegreeCard degree={degree} theme={theme} />;
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Educations;
